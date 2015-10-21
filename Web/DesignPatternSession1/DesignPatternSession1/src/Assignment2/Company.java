4e5d23b87ce0334b87718fba2f7cc4a77ad1564a
Manish Sharma <manish.sharma2@metacube.com>
2015-10-21 18:07:23 +0530

    Design&Pattern assignment1

---
+++ Web/DesignPatternSession1/DesignPatternSession1/src/Assignment2/Company.java
@@ -0,0 +1,58 @@
+package Assignment2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+Write an application to create a Company and its employees(Developers) without exposing their public constructors.
+ Implement Classes(Factory Pattern) like:
+Company ->Developer 
+class Developer{
+string Name   	{get;set;}
+string Role       	{get;set;}}
+class Company extend Developer  {
+public static List<Developer> getDevelopers(){}
+}
+
+*/
+
+/**
+ * @author Manish
+ * application to create a Company and its employees(Developers) without exposing their public constructors.
+ * Implement Classes(Factory Pattern) 
+ */
+public class Company {
+
+	private static List<Developer> devloperList;
+
+	/**
+	 * @param developer : to be added in devloperList
+	 */
+	public static void addDevloper(Developer developer) {
+		if (devloperList == null) {
+			devloperList = new ArrayList<Developer>();
+		}
+		devloperList.add(developer);
+	} 
+ 
+	/**
+	 * @param name : name of developer
+	 * @param role : role of developer
+	 * @return : instance of Developer
+	 */
+	public static Developer getDeveloper(String name, String role) {
+		Developer developer = DeveloperFactory.getDeveloper();
+		developer.setName(name);
+		developer.setRole(role);
+		return developer;
+	}
+
+	/**
+	 * @return : list of Developer, devloperList
+	 */
+	public static List<Developer> getDeveloperList() {
+		// DeveloperFactory.getDeveloper();
+		return devloperList;
+	}
+
+}
