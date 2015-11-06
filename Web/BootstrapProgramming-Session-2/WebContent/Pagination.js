bcf11403d3ccbbb900980ea8f4f511f13b35c3ca
Manish Sharma <manish.sharma2@metacube.com>
2015-11-06 18:37:35 +0530

    completed

--- Web/BootstrapProgramming-Session-2/WebContent/Pagination.js
+++ Web/BootstrapProgramming-Session-2/WebContent/Pagination.js
@@ -1,33 +1,108 @@
 $(document).ready(init);
 
 function init() {
-	$("#pg1").on('click', displayTable);
-	$("#pg2").on('click', displayTable1);
-	$("#pg3").on('click', displayTable2);
-	$("#pg4").on('click', displayTable3);
-	$("#pg5").on('click', displayTable4);
+	addTable(1);
+	document.getElementById("pg1").focus();
+	$('#searchButton').on('click',search);
 }
 
-function displayTable() {
-	var query = "<table style='border:1px solid black;'><tr><th>Movie Name</th><th>Movie Review</th></tr><tr><td>Jab We Met</td><td>4</td></tr><tr><td>3 Idiots</td><td>5</td></tr><tr><td>Shanghai</td><td>1</td></tr></table>";
-	document.getElementById("details").innerHTML = query;
+function search() {
+	var Title = document.getElementById('search').value;
+	//alert($('#search').text());
+	if(Title == ""){
+		alert("Please enter some movie name");
+		return;
+	} 
+	else {
+		for ( i = 0; i < movieData.length ; i++) {
+			if(Title == movieData[i].MovieTitle){
+				$('#movieTable tbody').children().remove();
+				var table = $('#movieTable tbody');
+				table.append('<tr><td>' + movieData[i].MovieTitle + '</td><td>'
+					+ movieData[i].Genre + '</td><td>' + movieData[i].Director
+					+ '</td></tr>');
+				return;
+			}
+		}
+		if(i==15){
+			alert("Movie Not Found");
+		}
+	}
+
 }
 
-function displayTable1() {
-	var query = "<table style='border:1px solid black;'><tr><th>Movie Name</th><th>Movie Review</th></tr><tr><td>Pyar ka panchnama</td><td>5</td></tr><tr><td>YJHD</td><td>4</td></tr><tr><td>Shandar</td><td>1</td></tr></table>";
-	document.getElementById("details").innerHTML = query;
+function addTable(size) {
+
+	movieData = [ {
+		MovieTitle : 'Goosebumps',
+		Genre : 'Action',
+		Director : 'DirectorA'
+	}, {
+		MovieTitle : 'Dinosaur Adventure',
+		Genre : 'Action',
+		Director : 'Director B'
+	}, {
+		MovieTitle : 'Wild Roller Coaster',
+		Genre : 'Action',
+		Director : 'Director C'
+	}, {
+		MovieTitle : 'Temple Run 7',
+		Genre : 'Action',
+		Director : 'Director D'
+	}, {
+		MovieTitle : 'Jab We Met',
+		Genre : 'Action',
+		Director : 'Director E'
+	}, {
+		MovieTitle : 'Pyar ka panchnama',
+		Genre : 'Action',
+		Director : 'Director F'
+	}, {
+		MovieTitle : 'Fashion',
+		Genre : 'Action',
+		Director : 'Director G'
+	}, {
+		MovieTitle : '3 Idiots',
+		Genre : 'Action',
+		Director : 'Director H'
+	}, {
+		MovieTitle : 'Shanghai',
+		Genre : 'Action',
+		Director : 'Director I'
+	}, {
+		MovieTitle : 'Shandar',
+		Genre : 'Action',
+		Director : 'Director J'
+	}, {
+		MovieTitle : 'Manjhi',
+		Genre : 'Action',
+		Director : 'Director K'
+	}, {
+		MovieTitle : 'HAK',
+		Genre : 'Action',
+		Director : 'Director L'
+	}, {
+		MovieTitle : 'YJHD',
+		Genre : 'Action',
+		Director : 'Director M'
+	}, {
+		MovieTitle : 'Pyar ka panchnama-2',
+		Genre : 'Action',
+		Director : 'Director N'
+	}, {
+		MovieTitle : 'Dhoom',
+		Genre : 'Action',
+		Director : 'Director O'
+	} ];
+
+	$('#movieTable tbody').children().remove();
+	var table = $('#movieTable tbody');
+	for (var i = (size * 3 - 3); i < size * 3; i++) {
+		table.append('<tr><td>' + movieData[i].MovieTitle + '</td><td>'
+				+ movieData[i].Genre + '</td><td>' + movieData[i].Director
+				+ '</td></tr>');
+	}
+
 }
 
-function displayTable2() {
-	var query = "<table style='border:1px solid black;'><tr><th>Movie Name</th><th>Movie Review</th></tr><tr><td>Fashion</td><td>3.5</td></tr><tr><td>Manjhi</td><td>3.5</td></tr><tr><td>HAK</td><td>2</td></tr></table>";
-	document.getElementById("details").innerHTML = query;
-}
 
-function displayTable3() {
-	var query = "<table style='border:1px solid black;'><tr><th>Movie Name</th><th>Movie Review</th></tr><tr><td>Jannat</td><td>3</td></tr><tr><td>Cocktail</td><td>3</td></tr><tr><td>Avengers</td><td>5</td></tr></table>";
-	document.getElementById("details").innerHTML = query;
-}
-function displayTable4() {
-	var query = "<table style='border:1px solid black;'><tr><th>Movie Name</th><th>Movie Review</th></tr><tr><td>Tanu Weds Manu</td><td>4.5</td></tr><tr><td>Fast and Furious</td><td>5</td></tr><tr><td>Golmal</td><td>4</td></tr></table>";
-	document.getElementById("details").innerHTML = query;
-}
